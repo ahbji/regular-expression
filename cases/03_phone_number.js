80e5cc5c24950194743125210c93e3a81a428db1
andreahbji <andrewpetrovic@gmail.com>
2022-02-03 10:44:47 +0800

    更新 phone number

--- cases/03_phone_number.js
+++ cases/03_phone_number.js
@@ -1,6 +1,7 @@
 // 第一个3位数字可以带也可以不带括号
 
 let phoneNumber = "707-827-0000";
-let wsRegex = /^(\(\d{3}\)|^\d{3}[.-]?)?\d{3}[.-]?\d{4}$/;
+//let wsRegex = /^(\(\d{3}\)|^\d{3}[.-]?)?\d{3}[.-]?\d{4}$/;
+let wsRegex = /^\(?(?:\d{3})\)?[-.]?(?:\d{3})[-.]?(?:\d{4})$/
 let result = wsRegex.test(phoneNumber);
 console.log(result)
